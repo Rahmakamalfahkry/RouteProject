c88c28c04cb6bc9d90f37a37affc215108d1aae1
Rahma Kamal <rahma.kamal.fakhry@gmail.com>
2025-12-12 00:54:30 +0200

    Remove println

--- src/day1/FirstJavaProgram.java
+++ src/day1/FirstJavaProgram.java
@@ -3,6 +3,5 @@ package day1;
 public class FirstJavaProgram {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
     }
 }
